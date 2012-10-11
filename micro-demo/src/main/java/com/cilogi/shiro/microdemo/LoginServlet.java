e924c41ea68862bb0bb08bde2120803b64ccad3c
Tim Niblett <tim.niblett@cilogi.com>
2012-10-12 00:18:16 +0100

    Adding in micro demo (not yet working)

---
+++ micro-demo/src/main/java/com/cilogi/shiro/microdemo/LoginServlet.java
@@ -0,0 +1,76 @@
+// Copyright (c) 2012 Tim Niblett. All Rights Reserved.
+//
+// File:        LoginServlet.java  (11-Oct-2012)
+// Author:      tim
+//
+// Copyright in the whole and every part of this source file belongs to
+// Tim Niblett (the Author) and may not be used, sold, licenced, 
+// transferred, copied or reproduced in whole or in part in 
+// any manner or form or in or on any media to any person other than 
+// in accordance with the terms of The Author's agreement
+// or otherwise without the prior written consent of The Author.  All
+// information contained in this source file is confidential information
+// belonging to The Author and as such may not be disclosed other
+// than in accordance with the terms of The Author's agreement, or
+// otherwise, without the prior written consent of The Author.  As
+// confidential information this source file must be kept fully and
+// effectively secure at all times.
+//
+
+
+package com.cilogi.shiro.microdemo;
+
+import com.cilogi.shiro.persona.DefaultPersonaUserDAO;
+import com.cilogi.shiro.persona.PersonaAuthenticationToken;
+import com.cilogi.shiro.persona.PersonaLogin;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.web.util.SavedRequest;
+import org.apache.shiro.web.util.WebUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+
+    private final PersonaLogin personaLogin;
+
+    public LoginServlet() {
+        personaLogin = new PersonaLogin(new DefaultPersonaUserDAO());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html><http lang=\"en\"><head></head><body><h1>Logging you in...</h1></body></http>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String token = WebUtils.getCleanParam(request, "token");
+
+            PersonaAuthenticationToken personaToken = new PersonaAuthenticationToken(token, "http://localhost:8080", true);
+            try {
+                personaLogin.login(personaToken);
+                SavedRequest savedRequest = WebUtils.getAndClearSavedRequest(request);
+                String redirectUrl = (savedRequest == null) ? "/index.html" : savedRequest.getRequestUrl();
+                response.sendRedirect(response.encodeRedirectURL(redirectUrl));
+            } catch (AuthenticationException e) {
+                issue("text/plain", HttpServletResponse.SC_NOT_FOUND, "cannot authorize token: " + token, response);
+            }
+        } catch (Exception e) {
+            issue("text/plain", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal error: " + e.getMessage(), response);
+        }
+    }
+
+    private void issue(String mimeType, int returnCode, String output, HttpServletResponse response) throws IOException {
+        response.setContentType(mimeType);
+        response.setStatus(returnCode);
+        response.getWriter().println(output);
+    }
+
+}
